084b6fca98de4dcea55501d36199aa77943b3a66
Max <maxgt734@gmail.com>
2021-12-22 23:39:31 -0600

    Se agregó una navbar y estilo a la pagina de Catalogo

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header';
 import Catalog from './components/Catalog'
+
 
 function App() {
 
   return (
-    <Catalog />
+    <div className="App">
+      <Header/>
+      <Catalog />
+    </div>
   );
 }
 
--- front-end/src/components/Catalog/index.js
+++ front-end/src/components/Catalog/index.js
@@ -1,22 +1,27 @@
 import React, { useState } from "react";
 
 const productosIniciales = [
-    {codigo: 1, name: "card1", image: "https://assets.findthemetaverse.com/1st%20edition%20emblem.jpg/BROK/resize/1200x1200%3E/format/jpg/quality/70/1st%20edition%20emblem.jpg", active: true, valor: 100},
-    {codigo: 2, name: "card2", image: "https://cdn.shopify.com/s/files/1/1700/9483/articles/Blastoise_Unlimited_8493eae6-afc8-4d39-a9e8-1a846ab9321e_345x@2x.jpg?v=1519010261", active: true, valor: 200},
-    {codigo: 3, name: "card3", image: "https://52f4e29a8321344e30ae-0f55c9129972ac85d6b1f4e703468e6b.ssl.cf2.rackcdn.com/products/pictures/203260.jpg", active: true, valor: 300}
+    {codigo: 1, name: "Gyarados", image: "https://assets.findthemetaverse.com/1st%20edition%20emblem.jpg/BROK/resize/1200x1200%3E/format/jpg/quality/70/1st%20edition%20emblem.jpg", active: true, valor: 100},
+    {codigo: 2, name: "Bulbasaur", image: "https://m.media-amazon.com/images/I/51jPhiOpGEL._AC_.jpg", active: true, valor: 200},
+    {codigo: 3, name: "Pikachu", image: "https://product-images.tcgplayer.com/fit-in/400x558/88101.jpg", active: true, valor: 300},
+    {codigo: 4, name: "Charizard", image: "https://m.media-amazon.com/images/I/71nbfl-JklS._AC_SL1024_.jpg", active: true, valor: 300},
+    {codigo: 5, name: "Chikorita", image: "https://m.media-amazon.com/images/I/51lT52ZV56L._AC_.jpg", active: true, valor: 200},
+    {codigo: 6, name: "Alakazam", image: "https://lh3.googleusercontent.com/proxy/q4PUFQOLQu09Ne_lsNTQmre7iaocNHhQM_gfmNaSk6OWfu4UODTVf43600cR87djwb2HwSyBPaESgwB2DB36TubLFtf0HJdV6ukRD2V8Fy77ECLQMMsViwQWWQ2nBkqfC5e7bRKQ-JLT8QobQnIgmxkNbWapKgMA", active: true, valor: 200},
+    {codigo: 7, name: "Blastoise", image: "https://52f4e29a8321344e30ae-0f55c9129972ac85d6b1f4e703468e6b.ssl.cf2.rackcdn.com/products/pictures/203260.jpg", active: true, valor: 300}
 ];
 
 const Header = () => {
-    return <h1> Hola buenas</h1>
+    return <h1 style={{ marginTop: 65, color: "white" }}> Hola buenas</h1>
 }
 
 const Producto = (prop) => {
     const valor = prop.valor || 0;
     return (
-        <div className="card w-100">
+        <div className="card w-100 text-white bg-dark">
             <img className="card-img-top h-50" src={prop.image} style={{minHeight:350, maxHeight:350}} alt=""/>
+            <div className="card-header">{prop.name}</div>
             <div className="card-body">
-                <h5 className="card-title text-center">{prop.name} ${valor}</h5>
+                <h5 className="card-title text-center">${valor}</h5>
                 <p className="text-center">Precio: ${(Math.round(32 * 100) / 100).toFixed(2)}</p>
                 <div className="row justify-content-center">
                     <div className="col-8">
@@ -35,11 +40,6 @@ const Producto = (prop) => {
 const Productos = (prop) => {
     return (
         <div className="mx-4" style={{ padding: 20 }}>
-            <div className="row">
-                <button className="btn btn-block btn-primary">
-                    Proceder a Pago
-                </button>
-            </div>
             <div className="row">
                 {prop.productos.map((item) => {
                     return (
